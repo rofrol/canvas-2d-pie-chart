c7730b000b120f361b1db5b6e706beee984c01f6
Roman Frołow <rofrol@gmail.com>
2015-12-02 21:50:30 +0100

    circle

--- index.js
+++ index.js
@@ -6,14 +6,6 @@ co.textBaseline = 'top';
 // nothing will show if default textBaseline (alphabetic) or ideographic, bottom and x and y 0
 co.fillText('hello', 0, 0);
 
-co.beginPath();
-co.moveTo(30, 30);
-co.lineTo(100, 100);
-co.lineWidth = 10;
-co.strokeStyle = 'red';
-co.lineCap = 'round';
-co.stroke();
-
 var x = ca.width / 2;
 var y = ca.height / 2;
 var radius = 75;
@@ -25,4 +17,17 @@ co.beginPath();
 co.arc(x, y, radius, startAngle, endAngle, counterClockwise);
 co.lineWidth = 15;
 co.strokeStyle = '#a31343';
+co.stroke();
+
+co.beginPath();
+co.arc(x, y, 80, 0, 2 * Math.PI, false);
+co.fillStyle = 'green';
+co.fill();
+
+co.beginPath();
+co.moveTo(30, 30);
+co.lineTo(100, 100);
+co.lineWidth = 10;
+co.strokeStyle = 'red';
+co.lineCap = 'round';
 co.stroke();
