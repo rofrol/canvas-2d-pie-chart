10b5b5c7f34950fb4f458932af0d22cafa77ad62
Roman Frołow <rofrol@gmail.com>
2015-12-02 21:33:40 +0100

    line

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
-var canvas = document.querySelector('#canvas');
-var context = canvas.getContext('2d');
-context.font = "20px sans-serif";
-context.fillStyle = "blue";
-context.textBaseline = "top";
+var ca = document.querySelector('#canvas');
+var co = ca.getContext('2d');
+co.font = "20px sans-serif";
+co.fillStyle = "blue";
+co.textBaseline = "top";
 // nothing will show if default textBaseline (alphabetic) or ideographic, bottom and x and y 0
-context.fillText("hello", 0, 0);
+co.fillText("hello", 0, 0);
+
+co.beginPath();
+co.moveTo(30, 30);
+co.lineTo(50, 50);
+co.stroke();
