982ccd2d0421bfa35bbde8010039e09f9f3d89f6
Roman Frołow <rofrol@gmail.com>
2015-12-02 21:38:34 +0100

    lineCap

--- index.js
+++ index.js
@@ -1,14 +1,15 @@
 var ca = document.querySelector('#canvas');
 var co = ca.getContext('2d');
-co.font = "20px sans-serif";
+co.font = '20px sans-serif';
 co.fillStyle = "blue";
-co.textBaseline = "top";
+co.textBaseline = 'top';
 // nothing will show if default textBaseline (alphabetic) or ideographic, bottom and x and y 0
-co.fillText("hello", 0, 0);
+co.fillText('hello', 0, 0);
 
 co.beginPath();
 co.moveTo(30, 30);
-co.lineTo(50, 50);
-co.lineWidth = 5;
+co.lineTo(100, 100);
+co.lineWidth = 10;
 co.strokeStyle = 'red';
+co.lineCap = 'round';
 co.stroke();
